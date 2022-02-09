7bac511331decac86200ae5b0db95480b3b4ed32
Zachary Sain <zachsain@gmail.com>
2022-02-09 12:10:19 -0600

    trouble with first deliverable

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import QuestionItem from './QuestionItem'
 
-function QuestionList() {
+function QuestionList() { 
+  const [triviaQuestions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:4000/questions')
+    .then(r => r.json())
+    .then(questions => (console.log(questions), setQuestions(questions)))
+  }, [])
+
+
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        <QuestionItem question={triviaQuestions} />
+        {/* display QuestionItem components here after fetching */}
+        </ul>
     </section>
   );
 }
